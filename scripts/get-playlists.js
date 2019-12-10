facaafbc60f1796a16ba43d76b2adf2fae608e04
tom <thomasmaxwellsmith@gmail.com>
2019-12-10 22:50:08 +0000

    request and make playlist json

--- scripts/get-playlists.js
+++ scripts/get-playlists.js
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import request from 'request';
+import request from 'request-promise-native';
+import fs from 'fs';
 
 import secrets from '../secrets.js';
 
@@ -18,39 +20,24 @@ const authOptions = {
   json: true,
 };
 
-const getMoreTracks = (options, callback, existingBody) => {
-  request.get(options, function(error, response, body) {
-    existingBody.tracks.items.push(...body.items);
-    existingBody.tracks.next = body.next;
-    getPlaylist(options, callback, existingBody);
-  });
-};
-
-const getPlaylist = (options, callback, body) => {
-  // if there are more tracks, call the url spotify gives us
-  if (body.tracks.next) {
-    getMoreTracks(
-      Object.assign({}, options, {
-        url: body.tracks.next,
-      }),
-      callback,
-      body
-    );
-  } else {
-    callback({body});
-  }
-};
-
-const pl2018 = '42ROzUApBgyUW8nuHGP6w7';
-
-const parsePlaylist = ({body}) => {
+const parsePlaylist = (playlist) => {
   return {
-    name: body.name,
-    id: body.id,
-    images: body.images,
-    tracks: body.tracks.items.map((item, index) => ({
+    name: playlist.name,
+    id: playlist.id,
+    images: playlist.images,
+    tracks: playlist.tracks.items.map((item, index) => ({
       rank: index,
-      artists: item.track.artists.map((artist) => ({name: artist.name})),
+      artists: item.track.album.artists.map((artist) => ({name: artist.name})),
+      features: item.track.artists.reduce((acc, artist) => {
+        if (
+          !item.track.album.artists.find(
+            (albumArtist) => albumArtist.name === artist.name
+          )
+        ) {
+          return [...acc, {name: artist.name}];
+        }
+        return acc;
+      }, []),
       id: item.track.id,
       name: item.track.name,
       images: item.track.album.images,
@@ -58,25 +45,48 @@ const parsePlaylist = ({body}) => {
   };
 };
 
-const showResult = (result) => {
-  console.log(JSON.stringify(parsePlaylist(result)));
+const getOptionsHof = (access_token) => (playlistId) => ({
+  url: `https://api.spotify.com/v1/playlists/${playlistId}`,
+  headers: {
+    Authorization: 'Bearer ' + access_token,
+  },
+  json: true,
+});
+
+const playlistIds = [
+  '3suz5U1sFKZAnB0jIQyq5w',
+  '6S0N2gqj84dFlf7zjDwmim',
+  '3n5yJnfKwkZSaaBYsRoB3O',
+  '0dWK4F4cPcaHv7Jxgmco0l',
+  '5zYnYGmdjvt1Mi0Y6URjxV',
+  '426nb2ik0jRVdJHdmA9RVH',
+  '23tQCP4LTxzXYkq1IX5uRZ',
+  '6ar2Tjv1wy3WwZx8qucFiq',
+  '42ROzUApBgyUW8nuHGP6w7',
+  '1pjTl4HGhrtTB2w1WVvcqS',
+  '2AQzOSsx2Rv8ofjg613Hrl',
+];
+
+const init = async () => {
+  try {
+    const {access_token} = await request.post(authOptions);
+    const getOptions = getOptionsHof(access_token);
+    const playlists = await Promise.all(
+      playlistIds.map(async (playlistId, i) => {
+        const playlist = await request.get(getOptions(playlistId));
+        return parsePlaylist(playlist, i);
+      })
+    );
+    playlists.forEach((playlist) => {
+      fs.writeFileSync(
+        `./public/playlists/${playlist.name}.json`,
+        JSON.stringify(playlist, null, 2)
+      );
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error: ', error);
+  }
 };
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    // use the access token to access the Spotify Web API
-    const options = {
-      url: `https://api.spotify.com/v1/playlists/${pl2018}`,
-      headers: {
-        Authorization: 'Bearer ' + body.access_token,
-      },
-      json: true,
-    };
-    request.get(options, function(error, response, body) {
-      getPlaylist(options, showResult, body);
-      // res.send({body});
-    });
-  } else {
-    console.error(error, response.statusCode);
-  }
-});
+init();
