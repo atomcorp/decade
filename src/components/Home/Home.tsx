d9e17339126916422b2818dedc5ce7ac896e9c08
tom <thomasmaxwellsmith@gmail.com>
2019-12-12 21:48:17 +0000

    get grain 👌

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,18 @@
 import React, {Suspense} from 'react';
 
 import Routes from 'components/Routes';
+import css from './Home.module.css';
 
 const Home: React.FC = () => (
-  <section>
-    <h1>2010s</h1>
-    <Suspense fallback={<div>Loading</div>}>
-      <Routes />
-    </Suspense>
-  </section>
+  <>
+    <div className={css.background}></div>
+    <section className={css.container}>
+      <h1>2010s</h1>
+      <Suspense fallback={<div>Loading</div>}>
+        <Routes />
+      </Suspense>
+    </section>
+  </>
 );
 
 export default Home;
