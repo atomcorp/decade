af9f05e8182c62db8398a024835880a87cb47a64
tom <thomasmaxwellsmith@gmail.com>
2019-12-17 22:22:00 +0000

    Updates

---
+++ precache-manifest.c8fd369a3c013be45fb45950a23f106d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f99955b1f4183adac0fe9c1d857594e",
+    "url": "/decade/index.html"
+  },
+  {
+    "revision": "a80c9895c54cb228c071",
+    "url": "/decade/static/css/main.a999ff03.chunk.css"
+  },
+  {
+    "revision": "5e327bf1c0a81528dc3b",
+    "url": "/decade/static/js/2.fa00cbd5.chunk.js"
+  },
+  {
+    "revision": "a7ac7e7a1910293612f05953225e3800",
+    "url": "/decade/static/js/2.fa00cbd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a80c9895c54cb228c071",
+    "url": "/decade/static/js/main.6b5eaf55.chunk.js"
+  },
+  {
+    "revision": "2aed134ed66bcc5a0200",
+    "url": "/decade/static/js/runtime-main.9da472cb.js"
+  }
+]);
